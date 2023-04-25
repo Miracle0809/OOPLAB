70e32943f11b15805f4f50922541fb45a1ade153
Miracle <89405309+Miracle0809@users.noreply.github.com>
2023-04-25 10:43:51 +0700

    Add files via upload

---
+++ FirstDialog.java
@@ -0,0 +1,9 @@
+//Nguyen Viet Thang - 20215245
+import javax.swing.JOptionPane;
+
+public class FirstDialog{
+    public static void main (String[] args){
+        JOptionPane.showMessageDialog(null, "helllllo");
+        System.exit(0);
+    }
+}
